d9b80014279057bc25a5ef6f886967f06f08e357
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-29 18:29:05 -0300

    Exercicio 1

---
+++ dia_1/script.js
@@ -0,0 +1,22 @@
+// Exercício 1
+const pessoaContrada = (nome, email) => {
+    return pessoa = {
+        nome,
+        email
+    }
+}
+// console.table(pessoaContrada('Gabriel Pereira', 'gabriel@pereira.com'));
+
+const newEmployees = () => {
+    const employees = {
+      id1: pessoaContrada('Pedro Guerra', 'pedroguerra@trybe.com'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+      id2: pessoaContrada('Luiza Drumond', 'luizadrumond@trybe.com'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+      id3: pessoaContrada('Carla Paiva', 'carlapaiva@trybe.com')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+    }
+    return console.log(employees);
+  };
+newEmployees();
+
+// Exercício 2
+// Exercício 3
+// Exercício 4
