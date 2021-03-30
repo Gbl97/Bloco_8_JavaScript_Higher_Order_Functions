5e587fb6d7052af7a686a7a12ebe98dc6315321b
Gabriel Pereira <g.pereira.a.m@gmail.com>
2021-03-30 15:06:57 -0300

    Exercicio 4

--- dia_1/script.js
+++ dia_1/script.js
@@ -1,25 +1,28 @@
 // Exercício 1
 const pessoaContrada = (nome) => {
     return pessoa = {
-        nome: nome,
+        name: nome,
         email: `${nome.split(' ').join('').toLowerCase()}@trybe.com`
     }
 }
 // console.table(pessoaContrada('Gabriel Pereira'));
 
-const newEmployees = (pessoaContrada) => {
+const newEmployees = (fn) => {
     const employees = {
-      id1: pessoaContrada('Pedro Guerra'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
-      id2: pessoaContrada('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
-      id3: pessoaContrada('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
+      id1: fn('Pedro Guerra'),// Nome: Pedro Guerra -> Chame sua função passando o nome Pedro Guerra como parâmetro
+      id2: fn('Luiza Drumond'),// Nome: Luiza Drumond -> Chame sua função passando o nome Luiza Drumond como parâmetro
+      id3: fn('Carla Paiva')// Nome: Carla Paiva -> Chame sua função passando o nome Carla Paiva como parâmetro
     }
     return employees;
   };
-console.log(newEmployees(pessoaContrada));
+  console.log(newEmployees(pessoaContrada));
+// console.log(newEmployees(pessoaContrada).id1);
 
 // // Exercício 2
 const verificaNumero = (number) => {
     let sorteio = Math.floor(Math.random()*5 + 1);
+    // ou Math.ceil(Math.random()*5);
+
     console.log(sorteio);
 
     if (sorteio === number) {
@@ -49,5 +52,32 @@ const validaResposta = (respostaUsuario) => {
     }
 }
 
-console.log(validaResposta ('qualquerCoisa') ('coisa'));
+console.log(validaResposta ('QualquerCoisa') ('coisa'));
+
 // Exercício 4
+const rightAnswers = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const studentAnswers = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const verificaResposta = (rightAnswers, studentAnswers) => {
+    let cont = 0;
+    for (let index = 0; index < rightAnswers.length; index += 1) {
+
+        if (studentAnswers[index] === 'N.A') {
+            cont += 0;
+        } else if(rightAnswers[index] === studentAnswers[index]) {
+            cont += 1;
+        } else {
+            cont -= 0.5;
+        }
+    }
+    return cont;
+
+}
+
+const respostaCorreta = (rightAnswers, studentAnswers, funcao) => {
+    funcao = verificaResposta(rightAnswers, studentAnswers);
+
+    return funcao;
+}
+
+console.log(respostaCorreta(rightAnswers, studentAnswers, verificaResposta));
